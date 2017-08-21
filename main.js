73a6ec8f82cf9ae0293e7a2ba82f9ae192405f5c
Benjamin Robson <unoceptik@gmail.com>
2017-08-21 09:45:23 +0300

    Added username input feature

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 window.onload = () => {
+  var html = document.querySelector('html');
+  html.style.backgroundImage = 'url(' + 'https://source.unsplash.com/category/buildings/?helsinki' + ')';
   ready();
 }
 
+
 function ready() {
 
-  var html = document.querySelector('html');
-  html.style.backgroundImage = 'url(' + 'https://source.unsplash.com/daily' + ')';
+  var input = document.getElementById('input');
+  var userName;
+
+  input.addEventListener('keydown', (e) => {
+    if (e.keyCode == 13) {
+      userName = input.value;
+      console.log(userName);
+      input.value = '';
+      e.preventDefault();
+      return false;
+    }
+  }, false)
 
   function startTime() {
     var d = new Date();
@@ -19,8 +32,4 @@ function ready() {
     }, 500);
   } startTime();
 
-  function user() {
-    var userName = document.getElementById('userName');
-    userName.innerHTML = 'Benjamin';
-  }
 }
